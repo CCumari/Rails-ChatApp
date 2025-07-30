d79ba1ee39b7e4f6b7c6c1c63e256aee5d9e9369
CCUmari <MUHAMMADUMAR12@CARECLOUD.COM>
2025-07-30 07:27:31 -0400

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+
+  def index
+    # This action will render the home page of the application.
+    # You can add any logic here that you want to execute when the home page is accessed.
+  end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+  def new
+    # This action will render the login page.
+    # You can add any logic here that you want to execute when the login page is accessed.
+  end
+
+  def create
+    # This action will handle the login logic.
+    # You can add authentication logic here to log in the user.
+  end
+
+  def destroy
+    # This action will handle the logout logic.
+    # You can add logic here to log out the user.
+  end
+end
